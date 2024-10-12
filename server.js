aa3f6c89f02f3fdb1b2b9665b06aa7ea3ad52521
Familia QM <felqueu@gmail.com>
2024-10-11 19:08:21 -0600

    se agregaron las rutas

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import "dotenv/config.js"
 import "./config/database.js"
+import cors from 'cors'
+import morgan from "morgan";
+import indexRouter from './router/index.js'
 
 const server = express()
+
 const PORT = process.env.PORT
 
-const ready = ()=> console.log("Server perritos ready in port: " +PORT)
+const ready = () => console.log("Server perritos ready in port: " + PORT)
 
-server.get('/',(request,response) =>{
-    response.send('Hola mundo en Express')
-})
-server.get('/saludo', (request,response) =>{
-    response.send('hola tonotos')
-})
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(cors())
+server.use(morgan('dev'))
 
+// router 
+server.use('/api', indexRouter)
 server.listen(PORT, ready)
+
+
 
